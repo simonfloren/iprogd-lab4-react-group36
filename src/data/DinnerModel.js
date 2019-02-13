30990683b2e968bf52006a3f8f2df97088a06059
Simon Florén <simflo@kth.se>
2019-02-13 16:02:41 +0100

    Added functions to model

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -1,15 +1,32 @@
-import ObservableModel from './ObservableModel';
+import ObservableModel from "./ObservableModel";
 
-const BASE_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com';
+const BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com";
 const httpOptions = {
-  headers: { 'X-Mashape-Key': '3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767' }
+  headers: {
+    "X-Mashape-Key": "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767"
+  }
 };
 
 class DinnerModel extends ObservableModel {
   constructor() {
     super();
-    this._numberOfGuests = 4;
+    this._numberOfGuests = 1;
     this.getNumberOfGuests();
+
+    this._menu = [];
+    this._types = [
+      "Main Course",
+      "Side Dish",
+      "Dessert",
+      "Appetizer",
+      "Salad",
+      "Bread",
+      "Breakfast",
+      "Soup",
+      "Beverage",
+      "Sauce",
+      "Drink"
+    ];
   }
 
   /**
@@ -29,6 +46,49 @@ class DinnerModel extends ObservableModel {
     this.notifyObservers();
   }
 
+  /**
+   * Add dish to menu stored in model
+   *
+   * @param {number} id
+   */
+  addDish(id) {
+    console.log("[dinnerModel] Added to menu, id:", id);
+
+    if (this._menu.some(menuItem => menuItem.id == id)) {
+      console.log("Dish already in menu");
+      return;
+    }
+
+    this.getDish(id).then(dish => {
+      this._menu.push(dish);
+      this.notifyObservers("menu");
+    });
+  }
+
+  /**
+   *
+   * @param {number} id
+   */
+  removeDish(id) {
+    this._menu = this._menu.filter(function(currentDish) {
+      return currentDish.id != id;
+    });
+  }
+
+  /**
+   *
+   */
+  getMenu() {
+    return this._menu;
+  }
+
+  /**
+   *
+   */
+  getDishTypes() {
+    return this._types;
+  }
+
   // API methods
 
   /**
@@ -40,11 +100,45 @@ class DinnerModel extends ObservableModel {
     return fetch(url, httpOptions).then(this.processResponse);
   }
 
+  /**
+   * API call to retrieve info about a certain dish
+   *
+   * @param {number} id
+   */
+  getDish(id) {}
+
   processResponse(response) {
     if (response.ok) {
       return response.json();
     }
     throw response;
+  }
+
+  getRandomDishes() {
+    const url = BASE_URL + "/recipes/random?number=" + 12;
+
+    return fetch(url, /* {
+      method: "GET",
+      headers: {
+        "X-Mashape-Key": API_KEY,
+        Accept: "application/json"
+      }
+    } */httpOptions)
+      .then(response => response.json())
+      .then(data => data.recipes);
+  }
+
+  /**
+   * Serialize an object into an url encoded string
+   * @param {Object object} params
+   */
+  serialize(params) {
+    var query = "";
+    for (let key in params) {
+      query +=
+        encodeURIComponent(key) + "=" + encodeURIComponent(params[key]) + "&";
+    }
+    return query;
   }
 }
 
