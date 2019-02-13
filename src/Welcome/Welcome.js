f5de1433898fb5d46402338890c03b3a8fb27950
Albin Winkelmann <albin-w@hotmail.com>
2019-02-13 14:02:46 +0100

    Changed to singlequotes

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Welcome.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Welcome.css';
 
 class Welcome extends Component {
   render() {
     return (
-      <div className="Welcome">
+      <div className='Welcome'>
         <p>Welcome to the dinner planner React Startup code!</p>
 
-        <Link to="/search">
+        <Link to='/search'>
           <button>Start planning</button>
         </Link>
       </div>
