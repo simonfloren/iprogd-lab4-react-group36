d8b4030f8a94543ea273e8eb5128a18023dce604
Simon Tran FLorén <simon.floren@gmail.com>
2019-02-18 19:03:50 +0100

    Changed base url

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -1,6 +1,6 @@
 import ObservableModel from "./ObservableModel";
 
-const BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com";
+const BASE_URL = "http://sunset.nada.kth.se:8080/iprog/group/36/recpies/search";
 const httpOptions = {
   headers: {
     "X-Mashape-Key": "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767"
